758c777d8efee44f7defcdd3e7cdc9d5b560a004
jianghurong <1164310634@qq.com>
2021-02-24 18:14:48 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,156 @@
 <template>
-  <div id="app">
-    <div class="container-left">
-      <div class="component-list">
-        <div v-for="(item, index) in leftComponents" :key="index">
-          <div class="component-title">
-            <a-icon type="apartment" />
-            {{ item.title }}
-          </div>
-          <a-row :gutter="[12, 12]" style="margin-bottom: 12px">
-            <draggable
-              :list="item.list"
-              class="component-draggable"
-              :group="{ name: 'componentsGroup', pull: 'clone', put: 'false' }"
-            >
-              <a-col :md="12" :sm="24" v-for="(component, componentIndex) in item.list" :key="componentIndex">
-                <div class="component-inner" @click="addComponent(component)">
-                  <a-icon :type="component.__config__.icon" />
-                  {{ component.__config__.label }}
+    <div id="app">
+        <div class="container-left">
+            <div class="component-list">
+                <div v-for="(item, index) in leftComponents" :key="index">
+                    <div class="component-title">
+                        <a-icon type="apartment" />
+                        {{ item.title }}
+                    </div>
+                    <a-row :gutter="[12, 12]" style="margin-bottom: 12px">
+                        <draggable
+                            :list="item.list"
+                            class="component-draggable"
+                            :group="{
+                                name: 'componentsGroup',
+                                pull: 'clone',
+                                put: 'false',
+                            }"
+                        >
+                            <a-col
+                                :md="12"
+                                :sm="24"
+                                v-for="(component, componentIndex) in item.list"
+                                :key="componentIndex"
+                            >
+                                <div
+                                    class="component-inner"
+                                    @click="addComponent(component)"
+                                >
+                                    <a-icon :type="component.__config__.icon" />
+                                    {{ component.__config__.label }}
+                                </div>
+                            </a-col>
+                        </draggable>
+                    </a-row>
                 </div>
-              </a-col>
-          </draggable>
-          </a-row>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="container-content">
-      <draggable
-        :list="drawingList"
-        :group="{ name: 'componentsGroup'}"
-      >
-        <div v-for="(item, index) in drawingList" :key="index">
-          <component :is="item.__config__.htmlTag">
-            123
-          </component>
-          {{ item }}
+        <div class="container-content">
+            <draggable :list="drawingList" :group="{ name: 'componentsGroup' }">
+                <a-form v-bind="formConfig">
+                    <div v-for="(item, index) in drawingList" :key="index">
+                        <a-form-item :label="item.__config__.label">
+                            <component
+                                :is="item.__config__.htmlTag"
+                                v-bind="getComponentAttribute(item)"
+                            >
+                                <div v-if="item.__slot__ !== undefined" v-html="buildRadioGroupChildren()">
+                                </div>
+                            </component>
+                        </a-form-item>
+                    </div>
+                </a-form>
+            </draggable>
+            <div v-show="!drawingList.length" class="content-empty-info">
+                从左侧拖入或点选组件进行表单设计
+            </div>
         </div>
-      </draggable>
-      <div v-show="!drawingList.length" class="content-empty-info">
-         从左侧拖入或点选组件进行表单设计
-      </div>
+        <!-- @todo 独立右侧模块 -->
+        <div class="container-right">
+            <right-menu
+                :activeComponent="activeComponent"
+                :formConfig="formConfig"
+            >
+
+            </right-menu>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
-import { inputComponents, selectComponents } from '@/components/generator/config.js'
+import { RightMenu } from '@/components/menu'
+import {
+    inputComponents,
+    selectComponents,
+    formConfig
+} from '@/components/generator/config.js'
 
 export default {
-  name: 'App',
-  components: {
-    draggable
-  },
-  data() {
-    return {
-      inputComponents,
-      drawingList: [],
-      leftComponents: [
-        { title: '输入型组件', list: inputComponents },
-        { title: '选择型组件', list: selectComponents }
-      ]
-    }
-  },
-  methods: {
-    // 添加组件（顺序）
-    addComponent(component) {
-      // 深拷贝组件
-      let clone = JSON.parse(JSON.stringify(component))
-      this.drawingList.push(clone)
-    }
-  }
-}
+    name: 'App',
+    components: {
+        draggable,
+        RightMenu
+    },
+    data() {
+        return {
+            ok: `<button>123</button>`,
+            inputComponents,
+            formConfig,
+            drawingList: [],
+            activeComponent: {},
+            leftComponents: [
+                { title: '输入型组件', list: inputComponents },
+                { title: '选择型组件', list: selectComponents },
+            ],
+        };
+    },
+    methods: {
+        // 添加组件（顺序）
+        addComponent(component) {
+            // 深拷贝组件
+            let clone = JSON.parse(JSON.stringify(component))
+            this.drawingList.push(clone)
+            this.activeComponent = clone
+        },
+        // 生成组件属性
+        getComponentAttribute(item) {
+            // @todo 删除__config__
+            return item
+        },
+        // 生成a-radio-group子级
+        buildRadioGroupChildren() {
+            return `<a-radio >123</a-radio><a-radio >245</a-radio>`
+        }
+    },
+};
 </script>
 
 <style lang="scss" scoped>
 #app {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  .container-left {
-    width: 300px;
-    height: 100%;
-    .component-list {
-      padding: 24px;
-      .component-title {
-        margin-bottom: 12px;
-      }
-      .component-draggable {
-        padding-bottom: 24px;
-      }
-      .component-inner {
-        padding: 8px;
-        background: #f5f5f5;
-        border: 1px solid transparent;
-        &:hover {
-          border: 1px dashed #1890ff;
-          color: #1890ff;
-          cursor: move;
-        }
-      }
-    }
-  }
-  .container-content {
     width: 100%;
-    height: 100%;
-  }
+    height: 100vh;
+    display: flex;
+    .container-left {
+        flex: 0 0 300px;
+        .component-list {
+            padding: 24px;
+            .component-title {
+                margin-bottom: 12px;
+            }
+            .component-draggable {
+                padding-bottom: 24px;
+            }
+            .component-inner {
+                padding: 8px;
+                background: #f5f5f5;
+                border: 1px solid transparent;
+                &:hover {
+                    border: 1px dashed #1890ff;
+                    color: #1890ff;
+                    cursor: move;
+                }
+            }
+        }
+    }
+    .container-content {
+        flex: auto;
+        height: 100%;
+    }
+    .container-right {
+        width: 350px;
+    }
 }
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
