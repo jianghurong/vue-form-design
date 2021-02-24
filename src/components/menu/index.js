3a0f798fd6e3c5df8cc5c15af6e87bfe0f079c2d
jianghurong <1164310634@qq.com>
2021-02-24 18:15:50 +0800

    组件入口

---
+++ src/components/menu/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Richard Chiang
+ * @Date: 2021-02-24 14:16:57
+ * @LastEditor: Richard Chiang
+ * @LastEditTime: 2021-02-24 14:27:58
+ * @Email: 19875991227@163.com
+ * @Description: 
+ */
+import RightMenu from './RightMenu'
+
+export {
+    RightMenu
+}
